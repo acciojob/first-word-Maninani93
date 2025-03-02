d760313086b4e726aa9b8ceb1b4c794aa4f47fdf
Maninani93 <javvadinani422@gmail.com>
2025-03-03 02:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function firstWord(s) {
   // your code here
+	let newStr = s.trim();
+    let index = newStr.indexOf(" ");
+    return index === -1 ? newStr: newStr.substring(0, index);
+	
 }
 
 // Do not change the code below
